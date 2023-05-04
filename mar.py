1ada3932ac30e1f9055a7edef6f7626c9a762b44
Default-Mat <matin.arno4646@gmail.com>
2023-05-04 20:52:44 +0430

    changed the main branch

--- mar.py
+++ mar.py
@@ -3,11 +3,16 @@ from Generated_files.MARParser import MARParser
 from Generated_files.MARLexer import MARLexer
 from MARCustomVisitor import MARCustomVisitor
 
-file = FileStream('sourceCode.txt')
-lexer = MARLexer(file)
-stream = CommonTokenStream(lexer)
-parser = MARParser(stream)
-tree = parser.program()
-visitor = MARCustomVisitor()
-visitor.visit(tree)
 
+def main():
+    file = FileStream('sourceCode.txt')
+    lexer = MARLexer(file)
+    stream = CommonTokenStream(lexer)
+    parser = MARParser(stream)
+    tree = parser.program()
+    visitor = MARCustomVisitor()
+    visitor.visit(tree)
+
+
+if __name__ == '__main__':
+    main()
