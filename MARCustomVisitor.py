0ce4cc44ef6ad0716e73dca99ce1ce141d2b2c9a
Reza TB <121418635+WexQuasExort@users.noreply.github.com>
2023-05-13 16:01:14 +0330

    Update MARCustomVisitor.py

--- MARCustomVisitor.py
+++ MARCustomVisitor.py
@@ -6,12 +6,29 @@ from antlr4 import *
 class MARCustomVisitor(MARVisitor):
     variables = {}
 
-    def visitAssignment(self, ctx:MARParser.AssignmentContext):
+    def visitLine(self, ctx: MARParser.LineContext):
+        if ctx.marStatement() is not None:
+            self.visitMarStatement(ctx.marStatement())
+
+        elif ctx.statement() is not None:
+            self.visitStatement(ctx.statement())
+
+        elif ctx.ifBlock() is not None:
+            self.visitIfBlock(ctx.ifBlock())
+
+        elif ctx.whileBlock() is not None:
+            self.visitWhileBlock(ctx.whileBlock())
+
+    def visitMarStatement(self, ctx: MARParser.MarStatementContext):
+        print("MAR: Matin Meskinnavaz, Amirreza Mehrabani, Reza Tahmasbi")
+
+    def visitAssignment(self, ctx: MARParser.AssignmentContext):
         varName = ctx.ID().getText()
         value = self.visit(ctx.expression())
+        print(value)
         self.variables[varName] = value
 
-    def visitConstant(self, ctx:MARParser.ConstantContext):
+    def visitConstant(self, ctx: MARParser.ConstantContext):
         if ctx.INTEGER() is not None:
             return int(ctx.INTEGER().getText())
 
@@ -29,22 +46,22 @@ class MARCustomVisitor(MARVisitor):
 
         raise Exception('Unknown value type')
 
-    def visitIdentifierExpression(self, ctx:MARParser.IdentifierExpressionContext):
+    def visitIdentifierExpression(self, ctx: MARParser.IdentifierExpressionContext):
         varName = ctx.ID().getText()
         if varName not in self.variables:
             raise Exception(f'Variable "{varName}" is not defined')
 
         return self.variables[varName]
 
-    def visitAddExpression(self, ctx:MARParser.AddExpressionContext):
+    def visitAddExpression(self, ctx: MARParser.AddExpressionContext):
         leftExp = self.visit(ctx.expression(0))
-        rightExpt = self.visit(ctx.expression(1))
+        rightExp = self.visit(ctx.expression(1))
         op = ctx.addOp().getText()
         if op == '+':
-            return self.__add(leftExp, rightExpt)
+            return self.__add(leftExp, rightExp)
 
         if op == '-':
-            return self.__subtract(leftExp, rightExpt)
+            return self.__subtract(leftExp, rightExp)
 
     def __add(self, left, right):
         if type(left) is int and type(right) is int:
@@ -80,3 +97,44 @@ class MARCustomVisitor(MARVisitor):
 
         else:
             raise Exception(f'You cannot subtract {type(left)} and {type(right)} value types')
+
+    # ifBlock
+    def visitIfBlock(self, ctx: MARParser.IfBlockContext):
+        if self.visit(ctx.expression()):
+            self.visit(ctx.block())
+            return
+        elif ctx.elseIfBlock() is not None:
+            for i in range(len(ctx.elseIfBlock())):
+                if self.visit(ctx.elseIfBlock(i).expression()):
+                    self.visit(ctx.elseIfBlock(i).block())
+                    return
+            if ctx.elseBlock() is not None:
+                self.visit(ctx.elseBlock().block())
+
+    # whileBlock
+    def visitWhileBlock(self, ctx: MARParser.WhileBlockContext):
+        while True:
+            if not self.visit(ctx.expression()):
+                break
+            try:
+                self.visit(ctx.block())
+            except ContinueException:
+                continue
+            except BreakException:
+                break
+
+    # while block breaker
+    def visitBreakStatement(self, ctx: MARParser.BreakStatementContext):
+        return 'break'
+
+    def visitContinueStatement(self, ctx: MARParser.ContinueStatementContext):
+        return 'continue'
+
+    # check if we are visiting an statement or an if-block
+#     def visitLine(self, ctx:MARParser.LineContext):
+#         if ctx.statement() is not None:
+#             return self.visit(ctx.statement())
+#         elif ctx.ifBlock() is not None:
+#             return self.visit(ctx.ifBlock())
+#         elif ctx.whileBlock() is not None:
+#             self.visit(ctx.whileBlock())
