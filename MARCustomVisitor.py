648f05b2d4e0b1c50cf200feea901e59fdcb13fe
Reza TB <121418635+WexQuasExort@users.noreply.github.com>
2023-05-28 20:12:33 +0330

    Update MARCustomVisitor.py

--- MARCustomVisitor.py
+++ MARCustomVisitor.py
@@ -6,27 +6,38 @@ from antlr4 import *
 class MARCustomVisitor(MARVisitor):
     variables = {}
 
-    def visitLine(self, ctx: MARParser.LineContext):
-        if ctx.marStatement() is not None:
-            self.visitMarStatement(ctx.marStatement())
+    def __init__(self):
+        self.should_break = False
+        self.should_continue = False
+        self.is_within_block = False
+        self.variables["pi"] = 3.14
+        self.variables["amirreza"] = 0
 
-        elif ctx.statement() is not None:
-            self.visitStatement(ctx.statement())
 
-        elif ctx.ifBlock() is not None:
-            self.visitIfBlock(ctx.ifBlock())
+    def visitFunctionCall(self, ctx: MARParser.FunctionCallExpressionContext):git pu
+        identify = ctx.ID().getText()
+        argument = []
+        if ctx.expression():
+            argument = [self.visit(expr) for expr in ctx.expression()]
 
-        elif ctx.whileBlock() is not None:
-            self.visitWhileBlock(ctx.whileBlock())
+        function = getattr(my_function, identify)
+        function(*argument)
 
-    def visitMarStatement(self, ctx: MARParser.MarStatementContext):
-        print("MAR: Matin Meskinnavaz, Amirreza Mehrabani, Reza Tahmasbi")
+    def visitFunctionCallExpression(self, ctx: MARParser.FunctionCallExpressionContext):
+        identify = ctx.functionCall().ID().getText()
+        argument = []
+        if ctx.functionCall().expression():
+            argument = [self.visit(expr) for expr in ctx.functionCall().expression()]
+
+        function = getattr(my_function, identify)
+        return function(*argument)
 
     def visitAssignment(self, ctx: MARParser.AssignmentContext):
         varName = ctx.ID().getText()
         value = self.visit(ctx.expression())
-        print(value)
         self.variables[varName] = value
+        print("test")
+        return None
 
     def visitConstant(self, ctx: MARParser.ConstantContext):
         if ctx.INTEGER() is not None:
@@ -39,7 +50,10 @@ class MARCustomVisitor(MARVisitor):
             return ctx.STRING().getText()
 
         if ctx.BOOL() is not None:
-            return bool(ctx.BOOL().getText())
+            if ctx.BOOL().getText() == "true":
+                return True
+            else:
+                return False
 
         if ctx.NULL() is not None:
             return None
@@ -55,13 +69,13 @@ class MARCustomVisitor(MARVisitor):
 
     def visitAddExpression(self, ctx: MARParser.AddExpressionContext):
         leftExp = self.visit(ctx.expression(0))
-        rightExp = self.visit(ctx.expression(1))
+        rightExpt = self.visit(ctx.expression(1))
         op = ctx.addOp().getText()
         if op == '+':
-            return self.__add(leftExp, rightExp)
+            return self.__add(leftExp, rightExpt)
 
         if op == '-':
-            return self.__subtract(leftExp, rightExp)
+            return self.__subtract(leftExp, rightExpt)
 
     def __add(self, left, right):
         if type(left) is int and type(right) is int:
@@ -98,43 +112,283 @@ class MARCustomVisitor(MARVisitor):
         else:
             raise Exception(f'You cannot subtract {type(left)} and {type(right)} value types')
 
-    # ifBlock
-    def visitIfBlock(self, ctx: MARParser.IfBlockContext):
-        if self.visit(ctx.expression()):
+    def visitMulExpression(self, ctx: MARParser.AddExpressionContext):
+        leftExp = self.visit(ctx.expression(0))
+        rightExp = self.visit(ctx.expression(1))
+        op = ctx.mulOp().getText()
+
+        if op == '*':
+            return self.__multy(leftExp, rightExp)
+        elif op == '/':
+            return self.__divied(leftExp, rightExp)
+        elif op == '%':
+            return self.__modify(leftExp, rightExp)
+        else:
+            raise Exception(f'Operation {op} not define')
+
+    def __multy(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left * right
+
+        elif type(left) is float and type(right) is float:
+            return left * right
+
+        elif type(left) is float and type(right) is int:
+            return left * right
+
+        elif type(left) is int and type(right) is float:
+            return left * right
+
+        elif type(left) is str and type(right) is int:
+            return left * right
+
+        elif type(left) is int and type(right) is str:
+            return left * right
+
+        else:
+            raise Exception(f'You cannot multiply {type(left)} and {type(right)} value types')
+
+    def __divied(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left / right
+
+        elif type(left) is float and type(right) is float:
+            return left / right
+
+        elif type(left) is float and type(right) is int:
+            return left / right
+
+        elif type(left) is int and type(right) is float:
+            return left / right
+
+        else:
+            raise Exception(f'You cannot divied {type(left)} and {type(right)} value types')
+
+    def __modify(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left % right
+
+        elif type(left) is float and type(right) is int:
+            return left % right
+
+        elif type(left) is float and type(right) is float:
+            return left % right
+
+        elif type(left) is int and type(right) is float:
+            return left % right
+
+        else:
+            raise Exception(f'You cannot modify {type(left)} and {type(right)} value types')
+
+    def visitBoolExpression(self, ctx: MARParser.AddExpressionContext):
+        leftExp = self.visit(ctx.expression(0))
+        rightExp = self.visit(ctx.expression(1))
+        op = ctx.boolOp().getText()
+
+        if op == 'and':
+            return self.__and(leftExp, rightExp)
+        elif op == 'or':
+            return self.__or(leftExp, rightExp)
+        else:
+            raise Exception(f'{op} is not a boolean Operation')
+
+    def __and(self, left, right):
+        if type(left) is int and type(right) is int:
+            return right
+        elif type(left) is float and type(right) is float:
+            return right
+
+        elif type(left) is float and type(right) is int:
+            return right
+
+        elif type(left) is int and type(right) is float:
+            return right
+
+        elif type(left) is bool and type(right) is bool:
+            return left and right
+        else:
+            raise Exception(f'can not in and')
+
+    def __or(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left
+        elif type(left) is float and type(right) is float:
+            return left
+
+        elif type(left) is float and type(right) is int:
+            return left
+
+        elif type(left) is int and type(right) is float:
+            return right
+
+        elif type(left) is bool and type(right) is bool:
+            return left or right
+        else:
+            raise Exception(f'can not in or')
+
+    def visitCompareExpression(self, ctx: MARParser.AddExpressionContext):
+        leftExp = self.visit(ctx.expression(0))
+        rightExp = self.visit(ctx.expression(1))
+        op = ctx.compareOp().getText()
+
+        if op == '<':
+            return self.__Smaller(leftExp, rightExp)
+        elif op == '>':
+            return self.__Bigger(leftExp, rightExp)
+        elif op == '<=':
+            return self.__SmallerEquals(leftExp, rightExp)
+        elif op == '>=':
+            return self.__GreaterEquals(leftExp, rightExp)
+        elif op == '==':
+            return self.__Equals(leftExp, rightExp)
+        elif op == '!=':
+            return self.__NotEquals(leftExp, rightExp)
+        else:
+            raise Exception(f'Operation {op} not define')
+
+    def __Smaller(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left < right
+
+        elif type(left) is float and type(right) is float:
+            return left < right
+
+        elif type(left) is float and type(right) is int:
+            return left < right
+
+        elif type(left) is int and type(right) is float:
+            return left < right
+        else:
+            raise Exception(f'you can not compare {type(left)} and {type(right)} value types')
+
+    def __Bigger(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left > right
+
+        elif type(left) is float and type(right) is float:
+            return left > right
+
+        elif type(left) is float and type(right) is int:
+            return left > right
+
+        elif type(left) is int and type(right) is float:
+            return left > right
+
+        else:
+            raise Exception(f'you can not compare {type(left)} and {type(right)} value types')
+
+    def __SmallerEquals(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left <= right
+
+        elif type(left) is float and type(right) is float:
+            return left <= right
+
+        elif type(left) is float and type(right) is int:
+            return left <= right
+
+        elif type(left) is int and type(right) is float:
+            return left <= right
+
+        else:
+            raise Exception(f'you can not compare {type(left)} and {type(right)} value types')
+
+    def __GreaterEquals(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left >= right
+
+        elif type(left) is float and type(right) is float:
+            return left >= right
+
+        elif type(left) is float and type(right) is int:
+            return left >= right
+
+        elif type(left) is int and type(right) is float:
+            return left >= right
+
+        else:
+            raise Exception(f'you can not compare {type(left)} and {type(right)} value types')
+
+    def __Equals(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left == right
+
+        elif type(left) is float and type(right) is float:
+            return left == right
+
+        elif type(left) is float and type(right) is int:
+            return left == right
+
+        elif type(left) is int and type(right) is float:
+            return left == right
+
+        else:
+            raise Exception(f'you can not compare {type(left)} and {type(right)} value types')
+
+    def __NotEquals(self, left, right):
+        if type(left) is int and type(right) is int:
+            return left != right
+
+        elif type(left) is float and type(right) is float:
+            return left != right
+
+        elif type(left) is float and type(right) is int:
+            return left != right
+
+        elif type(left) is int and type(right) is float:
+            return left != right
+
+        else:
+            raise Exception(f'you can not compare {type(left)} and {type(right)} value types')
+
+    def visitIfBlock(self, ctx):
+        conditionResult = self.visit(ctx.expression())
+        evaluatedBlock = False
+
+        if conditionResult:
+            evaluatedBlock = True
             self.visit(ctx.block())
-            return
-        elif ctx.elseIfBlock() is not None:
-            for i in range(len(ctx.elseIfBlock())):
-                if self.visit(ctx.elseIfBlock(i).expression()):
-                    self.visit(ctx.elseIfBlock(i).block())
-                    return
-            if ctx.elseBlock() is not None:
-                self.visit(ctx.elseBlock().block())
+        else:
+            elseIfBlock = ctx.elseIfBlock()
+            if elseIfBlock is not None:
+                elseIfResult = self.visit(elseIfBlock)
+                if elseIfResult:
+                    evaluatedBlock = True
+
+            if not evaluatedBlock:
+                elseBlock = ctx.elseBlock()
+                if elseBlock is not None:
+                    self.visit(elseBlock)
+
+        return None
+
+    def visitElseIfBlock(self, ctx):
+        conditionResult = self.visit(ctx.expression())
+        evaluatedBlock = False
+
+        if conditionResult:
+            evaluatedBlock = True
+            self.visit(ctx.block())
+        else:
+            elseIfBlock = ctx.elseIfBlock()
+            if elseIfBlock is not None:
+                elseIfResult = self.visit(elseIfBlock)
+                if elseIfResult:
+                    evaluatedBlock = True
+
+            if not evaluatedBlock:
+                elseBlock = ctx.elseBlock()
+                if elseBlock is not None:
+                    self.visit(elseBlock)
+
+        return True if evaluatedBlock else False
+
+    def visitElseBlock(self, ctx):
+        self.visit(ctx.block())
+        return True
 
     # whileBlock
     def visitWhileBlock(self, ctx: MARParser.WhileBlockContext):
-        while True:
-            if not self.visit(ctx.expression()):
-                break
-            try:
-                self.visit(ctx.block())
-            except ContinueException:
-                continue
-            except BreakException:
-                break
+        while self.visit(ctx.expression()):
+            self.visit(ctx.block())
 
-    # while block breaker
-    def visitBreakStatement(self, ctx: MARParser.BreakStatementContext):
-        return 'break'
-
-    def visitContinueStatement(self, ctx: MARParser.ContinueStatementContext):
-        return 'continue'
-
-    # check if we are visiting an statement or an if-block
-#     def visitLine(self, ctx:MARParser.LineContext):
-#         if ctx.statement() is not None:
-#             return self.visit(ctx.statement())
-#         elif ctx.ifBlock() is not None:
-#             return self.visit(ctx.ifBlock())
-#         elif ctx.whileBlock() is not None:
-#             self.visit(ctx.whileBlock())
